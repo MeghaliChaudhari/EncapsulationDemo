b83cd32cc203cf6d672a092fc2e7a1a39690a00f
Meghali Chaudhari <meghalichaudhari456@gmail.com>
2022-08-16 12:14:48 +0530

    completed

--- src/main/BookImpl.java
+++ src/main/BookImpl.java
@@ -1,4 +1,18 @@
 package main;
 
+import data.Book;
+
 public class BookImpl {
+    public static void main(String[] args) {
+        Book obj = new Book();
+        obj.setTitle("Book");
+        obj.setYearOfPublication(2003);
+        obj.setIsbn(1234567890123L);
+        obj.setAuthorName("ABC");
+
+        System.out.println("Title : " + obj.getTitle());
+        System.out.println("Year Of Publication : " + obj.getYearOfPublication());
+        System.out.println("Isbn no. : " + obj.getIsbn());
+        System.out.println("Author Name : " + obj.getAuthorName());
+    }
 }

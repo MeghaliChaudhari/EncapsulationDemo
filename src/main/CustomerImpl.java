bf9963b3e932910f32e5e16015697c65e3817411
Meghali Chaudhari <meghalichaudhari456@gmail.com>
2022-08-16 14:57:42 +0530

    completed project

--- src/main/CustomerImpl.java
+++ src/main/CustomerImpl.java
@@ -6,7 +6,16 @@ public class CustomerImpl {
     public static void main(String[] args) {
         Customer obj1 = new Customer("Meghali","XYZ","megha@gmail.com",1234567890,false);
         obj1.displayCustomer();
+
         Customer obj2 = new Customer();
+        obj2.displayCustomer();
+        Customer temp = obj1;
+        obj1 = obj2;
+        obj2 = temp;
+        System.out.println("No Argument");
+        obj1.displayCustomer();
+
+        System.out.println("Argument");
         obj2.displayCustomer();
 
 
